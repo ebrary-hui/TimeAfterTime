76538b7c11a2734c579793aad280329f89f7db75
Hui Chen <hui@ebrary.com>
2011-12-10 21:52:06 -0800

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by HUI CHEN on 12/10/11.
+//  Copyright (c) 2011 ebrary Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
